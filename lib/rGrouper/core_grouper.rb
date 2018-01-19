0bbb0717e70b9d8fd26f8dfbc94741574fde53a7
Karol Błaszczyk <karol.zygmunt.blaszczyk@gmail.com>
2018-01-19 14:25:56 +0100

    group merger

--- lib/rGrouper/core_grouper.rb
+++ lib/rGrouper/core_grouper.rb
@@ -4,24 +4,24 @@ module CoreGrouper
   # @param source [Array<Hash>]
   def group(source, groupings)
     groups = source.group_by { |el| group_proc(groupings.first).(el) || UNDEFINED_GROUP_NAME }
-
     groups.merge!(totals: summarizer.(groups.values.flatten)) if summarizer
-
-    totals_reject = -> (k, _v){ k == :totals }
-
-    groups.merge(groups.reject(&totals_reject)) do |_group, elements|
-      if groupings.count == 1
-        { values: elements }.tap do |hsh|
-          # totals only if more than 1 element
-          hsh.merge!(totals: summarizer.(elements)) if elements.count > 1 && summarizer
-        end
-      else
-        group(elements, groupings.drop(1))
-      end
+    groups.merge(groups.reject{|k, v| k == :totals }) do |_group, elements|
+      group_merger(elements, groupings)
     end
   end
 
   private
+
+  def group_merger(elements, groupings)
+    if groupings.count == 1
+      { values: elements }.tap do |hsh|
+        # totals only if more than 1 element
+        hsh.merge!(totals: summarizer.(elements)) if elements.count > 1 && summarizer
+      end
+    else
+      group(elements, groupings.drop(1))
+    end
+  end
 
   # @param grouping_by [Proc,String,Symbol]
   # @return [Proc]
